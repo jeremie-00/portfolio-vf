7a42e68b0218da4e72cb64ca7745349cedb34020
jeremie <jeremie.herault95@orange.fr>
2024-12-12 18:40:31 +0100

    type prisma

---
+++ src/types/prismaTypes.ts
@@ -0,0 +1,52 @@
+import { Prisma } from "@prisma/client";
+
+export type FullProject = Prisma.ProjectGetPayload<{
+  include: {
+    skills: true;
+    links: true;
+    cover: true;
+    medias: true;
+  };
+}>;
+
+export type FullSkill = Prisma.SkillGetPayload<{
+  include: {
+    image: true;
+    projects: true;
+  };
+}>;
+
+export type FullLink = Prisma.LinkGetPayload<{
+  include: {
+    icon: true;
+    project: true;
+  };
+}>;
+
+export type FullIcon = Prisma.IconGetPayload<{
+  include: {
+    Link: true;
+  };
+}>;
+
+export type FullSectionPage = Prisma.SectionPageGetPayload<{
+  include: {
+    image: true;
+    titles: true;
+    contents: true;
+  };
+}>;
+
+export type FullImageFile = Prisma.ImageFileGetPayload<{
+  include: {
+    skill: true;
+    coverProject: true;
+    mediasProject: true;
+    SectionPage: true;
+  };
+}>;
+
+export type ActionPrismaResponse = {
+  success?: string;
+  error?: string;
+};
