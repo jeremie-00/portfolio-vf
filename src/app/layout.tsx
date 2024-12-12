6067aa11275118fc5a9d6300d3064ceaf56261f0
jeremie <jeremie.herault95@orange.fr>
2024-12-12 19:00:09 +0100

    modification du theme global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { AppSidebar } from "./components/siderbar/appSidebarClient";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
