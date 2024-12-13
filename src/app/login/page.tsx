72ab780c1cc2173274771d8573e69f20df02cf05
jeremie <nerom95@gmail.com>
2024-12-13 13:12:40 +0100

    refactor page login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,44 +3,44 @@
 import { Button } from "@/components/ui/button";
 
 import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FaGithub } from "react-icons/fa"; // Import de l'icône GitHub
 import { TbLoader2 } from "react-icons/tb";
 
 export default function LoginPage() {
-  const router = useRouter();
   const { data: session, status } = useSession();
   const [isAuthenticating, setIsAuthenticating] = useState(false);
-  const [isRedirecting, setIsRedirecting] = useState(false);
-
-  useEffect(() => {
+  /* useEffect(() => {
     if (session) {
       setIsRedirecting(true); // Active l'état de redirection
       router.push("/dashboard/sections");
     }
-  }, [session, router]);
+  }, [session, router]); */
 
   const handleSignIn = async () => {
     setIsAuthenticating(true); // Active l'état d'authentification
     try {
-      await signIn("github");
-      setIsRedirecting(true);
+      await signIn("github", { callbackUrl: "/dashboard/sections" });
     } catch (error) {
       console.error("Erreur lors de l'authentification :", error);
-    } finally {
-      setIsAuthenticating(false); // Désactive l'état d'authentification
     }
   };
+  if (session)
+    return (
+      <div className="w-full h-full flex justify-center items-center text-3xl">
+        {" "}
+        hello {session.user.name}
+      </div>
+    );
 
   return (
     <div className="w-full h-full flex justify-center items-center">
       <Button
         onClick={handleSignIn}
-        disabled={status === "loading" || isAuthenticating || isRedirecting} // Désactive le bouton pendant l'authentification et la redirection
+        disabled={status === "loading" || isAuthenticating} // Désactive le bouton pendant l'authentification et la redirection
         className="flex items-center gap-2"
       >
-        {status === "loading" || isAuthenticating || isRedirecting ? (
+        {status === "loading" || isAuthenticating ? (
           <TbLoader2
             className="animate-spin"
             style={{ width: "26px", height: "26px" }}
