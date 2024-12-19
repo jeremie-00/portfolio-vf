081ef65342d1539bb68eb6c5cf5a295e0fd83bf7
jeremie <nerom95@gmail.com>
2024-12-19 16:17:52 +0100

    modification card projet

--- src/app/dashboard/projects/components/ProjectCard.tsx
+++ src/app/dashboard/projects/components/ProjectCard.tsx
@@ -1,8 +1,12 @@
 import { DeleteAlerteButton } from "@/app/components/Buttons";
-import { MotionCard } from "@/app/components/framer-motion/MotionCard";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { FullProject } from "@/types/prismaTypes";
 import { Pencil } from "lucide-react";
 import Image from "next/image";
@@ -40,76 +44,83 @@ export default function ProjectCard({ project, isAdmin }: ProjectCardProps) {
         validationErrors: result?.validationErrors,
       });
     }
+
     if (result?.data) {
       ToastProjectAction({ data: result.data, actionType });
     }
   };
+
   return (
-    <div
-      className="h-full w-full relative"
-      style={{
-        perspective: "1200px",
-      }}
-    >
-      {/* className="relative flex h-full flex-col transition duration-200 hover:shadow-xl hover:-translate-y-1 hover:border-primary" */}
-      <MotionCard isRotate={true}>
-        <div className="absolute flex flex-col top-0 left-0 w-full h-full items-center justify-center rounded-xl p-4 bg-black bg-opacity-80 text-white opacity-0 hover:opacity-100 transition-opacity duration-300">
-          {isAdmin ? (
-            <div className="flex items-center justify-center w-full gap-8 p-3 md:flex-row h-16">
-              <Link href={`/dashboard/projects/${project.id}`}>
-                <Button type="button" size="icon" variant="secondary">
-                  <Pencil
-                    style={{ width: "26px", height: "26px" }}
-                    color="#ffffff"
-                    strokeWidth={1.5}
-                    absoluteStrokeWidth
-                  />
-                </Button>
-              </Link>
-              <DeleteAlerteButton
-                actionButtonDelete={handleDeletedProject}
-                pendingDelete={isLoading}
+    <Card className="relative w-full h-full flex flex-col items-center justify-between bg-sidebar p-2 gap-2 hover:border-primary duration-300">
+      <div className="absolute flex flex-col top-0 left-0 w-full h-full items-center justify-center rounded-xl p-4 bg-black bg-opacity-40 text-white opacity-0 hover:opacity-100 transition-opacity duration-300">
+        {isAdmin ? (
+          <div className="flex items-center justify-center w-full gap-8 p-3 md:flex-row h-16">
+            <Link
+              className={buttonVariants({
+                variant: "secondary",
+                size: "icon",
+              })}
+              href={`/dashboard/projects/${project.id}`}
+            >
+              <Pencil
+                style={{ width: "26px", height: "26px" }}
+                color="#ffffff"
+                strokeWidth={1.5}
+                absoluteStrokeWidth
               />
-            </div>
-          ) : (
-            <Link href={`/pages/projects/${project.id}`}> Voir le projet </Link>
-          )}
+            </Link>
+            <DeleteAlerteButton
+              actionButtonDelete={handleDeletedProject}
+              pendingDelete={isLoading}
+            />
+          </div>
+        ) : (
+          <Link
+            className={buttonVariants({
+              variant: "outline",
+              size: "default",
+            })}
+            href={`/pages/projects/${project.id}`}
+          >
+            Voir le projet
+          </Link>
+        )}
+      </div>
+      <CardHeader>
+        {isAdmin ? (
+          <div className="w-full flex items-center justify-between">
+            <span>{project.order}</span>
+            <span>{project.type}</span>
+          </div>
+        ) : null}
+
+        {project.cover ? (
+          <div className="shadow-md w-full h-80 m-auto flex items-center justify-center border border-border rounded-xl overflow-hidden object-cover">
+            <Image
+              src={project.cover.url}
+              alt={project.cover.alt}
+              width={800}
+              height={420}
+              className="object-cover flex items-center justify-center rounded-lg"
+              priority={true}
+            />
+          </div>
+        ) : null}
+      </CardHeader>
+      <CardContent className="w-full flex flex-1 p-3 gap-6">
+        <div className="flex flex-col gap-2">
+          <h3 className="text-2xl font-bold">{project.title}</h3>
+          <hr className="w-20 border-primary border-[1.5px]"></hr>
+          <p>{project.shortDesc}</p>
         </div>
-        <CardHeader className="w-full flex-col flex items-center justify-between p-2">
-          {isAdmin ? (
-            <div className="w-full flex items-center justify-between">
-              <span>{project.order}</span>
-              <span>{project.type}</span>
-            </div>
-          ) : null}
-          <div className="h-[150px] w-full">
-            {project.cover ? (
-              <Image
-                src={project.cover.url}
-                alt={project.cover.alt}
-                width={400}
-                height={200}
-                className="object-cover w-full rounded-lg h-full w-full h-full"
-                priority={true}
-              />
-            ) : null}
-          </div>
-        </CardHeader>
-        <CardContent className="w-full flex flex-1 p-3 gap-6">
-          <div className="flex flex-col gap-2">
-            <h3 className="text-2xl font-bold">{project.title}</h3>
-            <hr className="w-20 border-primary"></hr>
-            <p className="flex text-gray-300 ">{project.shortDesc}</p>
-          </div>
-        </CardContent>
-        <CardFooter className="flex flex-wrap content-start w-full gap-2 p-3">
-          {project.skills.map((skill) => (
-            <Badge key={skill.id} variant="secondary" className="w-fit ">
-              {skill.title}
-            </Badge>
-          ))}
-        </CardFooter>
-      </MotionCard>
-    </div>
+      </CardContent>
+      <CardFooter className="w-full flex content-start gap-2 p-3">
+        {project.skills.map((skill) => (
+          <Badge key={skill.id} variant="default" className="w-fit">
+            {skill.title}
+          </Badge>
+        ))}
+      </CardFooter>
+    </Card>
   );
 }
