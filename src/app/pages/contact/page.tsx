c324d8b67e3bde27e6910fe62d8a9839d607b023
jeremie <nerom95@gmail.com>
2024-12-19 18:51:00 +0100

    page contact

---
+++ src/app/pages/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      Cette page sera disponible très bientôt !
+    </div>
+  );
+}
