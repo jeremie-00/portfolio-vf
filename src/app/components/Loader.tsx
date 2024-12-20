7791419d1e44330ba4c4553b546653b8246f23d0
jeremie <nerom95@gmail.com>
2024-12-20 10:13:27 +0100

    ajout des pages loading avec loader

---
+++ src/app/components/Loader.tsx
@@ -0,0 +1,37 @@
+"use client";
+const Loader = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        //backgroundColor: "#f9f9f9",
+      }}
+    >
+      <div
+        style={{
+          border: "4px solid #ccc",
+          borderTop: "4px solid #333",
+          borderRadius: "50%",
+          width: "100px",
+          height: "100px",
+          animation: "spin 1s linear infinite",
+        }}
+      />
+      <style jsx>{`
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Loader;
---
+++ src/app/pages/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from "../components/Loader";
+
+export default function Loading() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <Loader />
+    </div>
+  );
+}
