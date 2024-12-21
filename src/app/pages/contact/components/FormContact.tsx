174a985794c8f562b2f65ab9f30f80b8959fb1fc
jeremie <nerom95@gmail.com>
2024-12-21 12:19:02 +0100

    formulaire de contact

---
+++ src/app/pages/contact/components/FormContact.tsx
@@ -0,0 +1,121 @@
+"use client";
+import { IconLoaderCircle } from "@/app/dashboard/icons/components/DynamicIcon";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@radix-ui/react-label";
+import Form from "next/form";
+import { useState } from "react";
+import { useFormStatus } from "react-dom";
+
+export default function FormContact() {
+  const [formData, setFormData] = useState({
+    lastName: "",
+    firstName: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const BtnSubmit = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button type="submit" disabled={pending} size="default">
+        {pending ? <IconLoaderCircle /> : "Envoyer"}
+      </Button>
+    );
+  };
+
+  const handleSubmit = () => {
+    console.log("submit");
+    console.log(formData);
+  };
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-col">
+        <CardTitle className="px-4 py-8 text-center">
+          <h1 className=" text-2xl text-primary">Contactez-moi</h1>
+          <span className="block p-4 text-sm max-w-md">
+            Les informations que vous soumettez via ce formulaire sont
+            uniquement utilisées pour vous répondre. Elles ne sont pas
+            enregistrées dans une base de données et ne seront pas transmises à
+            des tiers.
+          </span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form action={handleSubmit} className="flex flex-col w-full gap-12">
+          <div className="flex flex-col md:flex-row gap-4">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="lastName" className="pl-2">
+                Nom
+              </Label>
+              <Input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="Doe"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="firsName" className="pl-2">
+                Prenom
+              </Label>
+              <Input
+                type="text"
+                name="firstName"
+                id="firsName"
+                placeholder="John"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="email" className="pl-2">
+              Email
+            </Label>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email@exemple.com"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="message" className="pl-2">
+              Message
+            </Label>
+            <Textarea
+              name="message"
+              id="message"
+              placeholder="Votre message..."
+              className="w-full"
+              value={formData.message}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="flex justify-end gap-4">
+            <BtnSubmit />
+          </div>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
